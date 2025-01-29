c324aba9ff270d6e1715447fb721008593549d01
Victor Inawo <inawovictor004@gmail.com>
2025-01-29 15:51:59 +0100

    fixed main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import DataTable from 'vue3-easy-data-table';
+import EasyDataTable from 'vue3-easy-data-table';
 import 'vue3-easy-data-table/dist/style.css';
 
 const app = createApp(App)
 
-app.component('EasyDataTable', DataTable)
+app
+.component('EasyDataTable', EasyDataTable)
 .mount('#app')
